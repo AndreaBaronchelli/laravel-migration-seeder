c7d548d48156aa0d4d2d69d47823794edee6d455
Andrea Baronchelli <baronchelli.andrea@gmail.com>
2021-06-11 00:03:49 +0200

    add bonus

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Travel;
 
 class PageController extends Controller
 {
@@ -10,6 +11,7 @@ class PageController extends Controller
      * HOMEPAGE
      */
     public function index() {
-       return view('home');
+        $travels = Travel::simplePaginate(5);
+        return view('home', compact('travels'));
    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts/main')
 
 @section('content')
-    MAIN CONTENT HERE
+    <div class="travels">
+        @foreach ($travels as $travel )
+            <div class="travel">
+                <h3>{{$travel->title}}</h3>
+                <div>{{$travel->days}} giorni</div>
+                <div>{{$travel->description}}</div>
+                <div><strong>{{$travel->price}}</strong></div>
+                @if ($travel->isConfirmed == true)
+                    <div>PARTENZA CONFERMATA!</div>
+                @else <div> Partenza in attesa di conferma</div>
+                @endif
+                @if (!$loop->last)
+                  <hr>  
+                @endif
+            </div>
+        @endforeach
+    </div>
+    <div class="nav">
+        {{$travels->links()}}
+    </div>
 @endsection
